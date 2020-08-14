f2691d01a4fb867e88e749282d43cd3267fe06a3
thh <duecorda@gmail.com>
2020-08-14 23:40:40 +0900

    정상상태 스킵

--- rdico.rb
+++ rdico.rb
@@ -1,33 +1,43 @@
 #!/usr/bin/env ruby
 
-should_restart = false
-
 drb_log_path = "/opt/www/l2mh/services/drb.log"
 dat = `tail -n 50 #{drb_log_path}`
 lines = dat.split(/[\r\n]/)
+
+should_restart = false
 lines.reverse.each do |line|
-  if /^\[ERROR/.match(line)
+  if /^\[INFO.*Discord.using.gateway.protocol.version:.[0-9],.requested:.[0-9]$/.match(line)
+    return puts "## Normal"
+  elsif /^\[ERROR/.match(line)
     should_restart = true
     break
   end
 end
 
 if should_restart
+  puts "## Shold restart"
   clue = `ps aux | grep 'rake.*_dicobot' | grep -v grep`
-  if clue.blank?
-    puts "####"
-    puts "## Not Found Process"
-    puts "####"
-  else
-    items = clue.split(/\s+/)
-    pid = items[1]
-    prs = items[-1]
-
-    shard_id = /\[([0-9]+),/.match(prs).to_a.last
-    max = /\[[0-9]+,([0-9]+)\]/.match(prs).to_a.last
-
-    `kill -SIGHUP #{pid}`
-    sleep(1)
-    `/home/ubuntu/.rbenv/shims/rake live_dicobot[#{shard_id},#{max}] >/opt/www/l2mh/services/drb.log 2>&1 &`
+  if clue.nil? || clue.empty?
+    return puts "## Not Found Process"
   end
+
+  items = clue.split(/\s+/)
+  pid = items[1]
+  prs = items[-1]
+
+  if pid.nil? || pid.empty? || prs.nil? || prs.empty?
+    return puts "## Not Found. pid: #{pid}, prs: #{prs}"
+  end
+
+  shard_id = /\[([0-9]+),/.match(prs).to_a.last
+  max = /\[[0-9]+,([0-9]+)\]/.match(prs).to_a.last
+
+  if shard_id.nil? || max.nil?
+    return puts "## Not Found. shard_id: #{shard_id}, max: #{max}"
+  end
+
+  `kill -SIGHUP #{pid}`
+  sleep(1)
+  `/home/ubuntu/.rbenv/shims/rake live_dicobot[#{shard_id},#{max}] >/opt/www/l2mh/services/drb.log 2>&1 &`
+  puts "## Done"
 end
