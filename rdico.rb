ee6b9d896ccdc2e6bb9d8aa409a6c8d5cd9c6529
thh <duecorda@gmail.com>
2020-08-20 19:19:05 +0900

    rdico 프로세스 없으면 재시작 루틴 추가

--- rdico.rb
+++ rdico.rb
@@ -1,43 +1,46 @@
 #!/usr/bin/env ruby
 
-drb_log_path = "/opt/www/l2mh/services/drb.log"
+SHARD_ID = ARGV[0]
+MAX_SHARD = ARGV[1]
+if SHARD_ID.nil? || MAX_SHARD.nil? || SHARD_ID.empty? || MAX_SHARD.empty?
+  return puts "## No shard_id and max_shard"
+end
+
+APP_ROOT = ENV['RAILS_ENV'] == "production" ? "/opt/www/l2mh" : "/home/duecorda/Projects/l2mh"
+WHICH_RAKE = ENV['RAILS_ENV'] == "production" ? "/home/ubuntu/.rbenv/shims/rake" : "/home/duecorda/.rbenv/shims/rake"
+RAKE_FILE = ENV['RAILS_ENV'] == "production" ? "live_dicobot" : "dev_dicobot"
+
+def restart(pid=nil)
+  `cp #{APP_ROOT}/services/drb.log #{APP_ROOT}/services/drb.bak`
+  `cp #{APP_ROOT}/services/dico.log #{APP_ROOT}/services/dico.bak`
+  if !pid.nil?
+    `kill -1 #{pid}`
+    sleep(1)
+  end
+  `cd #{APP_ROOT};#{WHICH_RAKE} #{RAKE_FILE}[#{SHARD_ID},#{MAX_SHARD}] >#{APP_ROOT}/services/drb.log 2>&1 &`
+  puts "## Restarted"
+end
+
+pids = `ps x | grep 'rake.*_dicobot' | grep -v grep | awk '{ print $1 }'`
+pid = pids.split(/[\r\n]/)
+if pid.length > 0
+  puts "## Multiple Process"
+  pid.each {|x| `kill -9 #{x}`}
+  return restart 
+elsif pid.length == 0
+  puts "## No Process"
+  return restart 
+end
+
+drb_log_path = "#{APP_ROOT}/services/drb.log"
 dat = `tail -n 50 #{drb_log_path}`
 lines = dat.split(/[\r\n]/)
 
-should_restart = false
 lines.reverse.each do |line|
   if /^\[INFO.*Discord.using.gateway.protocol.version:.[0-9],.requested:.[0-9]$/.match(line)
     return puts "## Normal"
   elsif /^\[ERROR/.match(line)
-    should_restart = true
-    break
+    puts line
+    return restart(pid)
   end
-end
-
-if should_restart
-  puts "## Shold restart"
-  clue = `ps aux | grep 'rake.*_dicobot' | grep -v grep`
-  if clue.nil? || clue.empty?
-    return puts "## Not Found Process"
-  end
-
-  items = clue.split(/\s+/)
-  pid = items[1]
-  prs = items[-1]
-
-  if pid.nil? || pid.empty? || prs.nil? || prs.empty?
-    return puts "## Not Found. pid: #{pid}, prs: #{prs}"
-  end
-
-  shard_id = /\[([0-9]+),/.match(prs).to_a.last
-  max = /\[[0-9]+,([0-9]+)\]/.match(prs).to_a.last
-
-  if shard_id.nil? || max.nil?
-    return puts "## Not Found. shard_id: #{shard_id}, max: #{max}"
-  end
-
-  `kill -SIGHUP #{pid}`
-  sleep(1)
-  `cd /opt/www/l2mh;/home/ubuntu/.rbenv/shims/rake live_dicobot[#{shard_id},#{max}] >/opt/www/l2mh/services/drb.log 2>&1 &`
-  puts "## Done"
 end
